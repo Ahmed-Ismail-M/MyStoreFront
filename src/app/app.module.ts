09e4a89255df633d66f280179663e8e0eb41e19d
aimthecoder <ahmedismailm85@gmail.com>
2022-06-12 07:10:05 +0200

    add product module and com

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
+import { ProductsModule } from './products/products.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, ProductsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
