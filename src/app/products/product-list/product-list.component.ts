d59b5f964ceffac91c3321ab9ffd62fd0cad88f1
ahmed <ahmedismailm85@gmail.com>
2022-06-12 12:34:01 +0200

    format doc

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -4,17 +4,23 @@ import { Product } from '../models/product.model';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
-  productList: Product[] = []
+  productList: Product[] = [];
   constructor() {
     this.productList = [
-      {id:1, name:'test', category:'test cat', price:2.4, img_url:`https://picsum.photos/id/${Math.floor(Math.random() * 20) + 1}/200/300`}
-    ]
-   }
-
-  ngOnInit(): void {
+      {
+        id: 1,
+        name: 'test',
+        category: 'test cat',
+        price: 2.4,
+        img_url: `https://picsum.photos/id/${
+          Math.floor(Math.random() * 20) + 1
+        }/100/100`,
+      },
+    ];
   }
 
+  ngOnInit(): void {}
 }
