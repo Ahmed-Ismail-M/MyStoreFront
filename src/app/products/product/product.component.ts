0a7a56bff28f97eebd77e0a1e772852934c25345
aimthecoder <ahmedismailm85@gmail.com>
2022-06-12 10:14:56 +0200

    add input product in child

--- src/app/products/product/product.component.ts
+++ src/app/products/product/product.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from '../models/product.model';
 
 @Component({
   selector: 'app-product',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-
+  @Input()
+  product!: Product;
   constructor() { }
 
   ngOnInit(): void {
