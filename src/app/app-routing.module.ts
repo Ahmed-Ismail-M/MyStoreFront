8bf7e60980d564dc8db724614fa8ee01f005b59c
ahmed <ahmedismailm85@gmail.com>
2022-06-16 15:22:22 +0200

    add path and confirmation c

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './products/product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'', component:ProductListComponent}, {path:'confirmation', component:Confirma}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { Random } from './utils/random';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ConfirmationComponent } from './confirmation/confirmation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ConfirmationComponent
   ],
   imports: [
     BrowserModule,
