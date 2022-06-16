c0821d0cced8c67960b58114e4c5498bce3dfaf8
ahmed <ahmedismailm85@gmail.com>
2022-06-16 13:49:06 +0200

    format doc

--- src/app/products/product/product.component.ts
+++ src/app/products/product/product.component.ts
@@ -4,18 +4,16 @@ import { Product } from '../models/product.model';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
   @Input()
   product!: Product;
-  @Output() hideProduct: EventEmitter<Product> = new EventEmitter;
-  constructor() { }
+  @Output() hideProduct: EventEmitter<Product> = new EventEmitter();
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  hide(product: Product) {
+    this.hideProduct.emit(product);
   }
-  hide(product: Product){
-    this.hideProduct.emit(product)
-  }
-
 }
