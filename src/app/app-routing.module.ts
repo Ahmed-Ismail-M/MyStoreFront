389cd2d98d4c84191aaabb4c6d62bc6558d56c9c
ahmed <ahmedismailm85@gmail.com>
2022-06-16 15:22:40 +0200

    add confirmation route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConfirmationComponent } from './confirmation/confirmation.component';
 import { ProductListComponent } from './products/product-list/product-list.component';
 
-const routes: Routes = [{path:'', component:ProductListComponent}, {path:'confirmation', component:Confirma}];
+const routes: Routes = [{path:'', component:ProductListComponent}, {path:'confirmation', component:ConfirmationComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
