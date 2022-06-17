5dd3b034b98f2db19ab844706f43eba2744b1e41
ahmed <ahmedismailm85@gmail.com>
2022-06-17 19:51:31 +0200

    format doc

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,14 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  tabs = [{'label':"Main", 'path':''}, {'label':'Todo', 'path':'confirmation'}]
+  tabs = [
+    { label: 'Main', path: '' },
+    { label: 'Todo', path: 'confirmation' },
+  ];
   activeLink = this.tabs[0].label;
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
