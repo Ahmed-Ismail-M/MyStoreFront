56b1af29b80e7e75f2f22327058d5d7639032e0a
ahmed <ahmedismailm85@gmail.com>
2022-06-17 19:52:41 +0200

    format doc

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,43 +3,41 @@ import { Product } from '../products/models/product.model';
 import { Random } from '../utils/random';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-
-  constructor(private random: Random) { }
-  getProducts(): Product[]{
+  constructor(private random: Random) {}
+  getProducts(): Product[] {
     return [
-      
       {
         id: this.random.randomNo(2000, 1),
         name: this.random.randomString(),
         category: this.random.randomString(),
-        price:this.random.randomNo(2000, 1),
+        price: this.random.randomNo(2000, 1),
         img_url: `https://picsum.photos/id/${
           this.random.randomNo(100, 1) + 1
         }/300/300`,
-        descr : this.random.randomString()
+        descr: this.random.randomString(),
       },
       {
-        id:this.random.randomNo(2000, 1),
+        id: this.random.randomNo(2000, 1),
         name: this.random.randomString(),
         category: this.random.randomString(),
-        price:this.random.randomNo(2000, 1),
+        price: this.random.randomNo(2000, 1),
         img_url: `https://picsum.photos/id/${
           this.random.randomNo(2000, 1) + 1
         }/300/300`,
-        descr : this.random.randomString()
+        descr: this.random.randomString(),
       },
       {
-        id:this.random.randomNo(2000, 1),
+        id: this.random.randomNo(2000, 1),
         name: this.random.randomString(),
         category: this.random.randomString(),
-        price:this.random.randomNo(2000, 1),
+        price: this.random.randomNo(2000, 1),
         img_url: `https://picsum.photos/id/${
           this.random.randomNo(2000, 1) + 1
         }/300/300`,
-        descr : this.random.randomString()
+        descr: this.random.randomString(),
       },
     ];
   }
