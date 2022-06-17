8732b9fb12b390d35da42dedba3dc0a7110d2c5c
ahmed <ahmedismailm85@gmail.com>
2022-06-17 06:07:18 +0200

    add navigation to tabs

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   links = ['Products', 'Todo'];
+  tabs = {'Main':"", 'todo':'confirmation'}
   activeLink = this.links[0];
   constructor() { }
 
