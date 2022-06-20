4f340b5542f7bcd3706c2991d98234f13a1a375b
ahmed <ahmedismailm85@gmail.com>
2022-06-20 12:50:17 +0200

    remove commented data

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -13,35 +13,3 @@ export class ProductService {
     return this.http.get<Product[]>('http://127.0.0.1:4200/assets/data.json')
   }
 }
-// [
-//   {
-//     id: this.random.randomNo(2000, 1),
-//     name: this.random.randomString(),
-//     category: this.random.randomString(),
-//     price: this.random.randomNo(2000, 1),
-//     img_url: `https://picsum.photos/id/${
-//       this.random.randomNo(100, 1) + 1
-//     }/300/300`,
-//     descr: this.random.randomString(),
-//   },
-//   {
-//     id: this.random.randomNo(2000, 1),
-//     name: this.random.randomString(),
-//     category: this.random.randomString(),
-//     price: this.random.randomNo(2000, 1),
-//     img_url: `https://picsum.photos/id/${
-//       this.random.randomNo(2000, 1) + 1
-//     }/300/300`,
-//     descr: this.random.randomString(),
-//   },
-//   {
-//     id: this.random.randomNo(2000, 1),
-//     name: this.random.randomString(),
-//     category: this.random.randomString(),
-//     price: this.random.randomNo(2000, 1),
-//     img_url: `https://picsum.photos/id/${
-//       this.random.randomNo(2000, 1) + 1
-//     }/300/300`,
-//     descr: this.random.randomString(),
-//   },
-// ];
