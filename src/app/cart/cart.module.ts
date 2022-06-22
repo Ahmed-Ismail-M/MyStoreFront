616e4fe6a9e25b90490a26e9b5272548780ebbcb
ahmed <ahmedismailm85@gmail.com>
2022-06-22 06:07:51 +0200

    add comp cart item

--- src/app/cart/cart.module.ts
+++ src/app/cart/cart.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartListComponent } from './cart-list/cart-list.component';
+import { CartItemComponent } from './cart-item/cart-item.component';
 
 
 
 @NgModule({
-  declarations: [CartListComponent],
+  declarations: [CartListComponent, CartItemComponent],
   imports: [
     CommonModule
   ],
