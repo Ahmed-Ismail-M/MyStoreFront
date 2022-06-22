fbcc4b78cecf221e7e7b672e72e1653ff2957b0b
ahmed <ahmedismailm85@gmail.com>
2022-06-22 19:09:22 +0200

    remove unused functions

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -8,22 +8,14 @@ import { Product } from '../models/product.model';
 export class CartService {
   cart: CartItem[] = JSON.parse(localStorage.getItem('cart') || '[]');
   constructor() {}
-  add(product: Product, qty: number): CartItem[] {
-    if (this.cart.length > 0) {
-      for (let item of this.cart) {
-        if (item.product.id == product.id) {
-          return this.increament(item, qty);
-        }
-      }
-    }
-    this.cart.push({ product: product, qty: qty });
-    this.saveCart();
-    return this.cart;
-  }
   addItem(item:CartItem):CartItem{
     if (this.cart.length > 0) {
       for (let cartItem of this.cart) {
         if (item.product.id == cartItem.product.id) {
+          if (item.qty < 1){
+            this.remove(item)
+            return item
+          }
           cartItem.qty = item.qty
           this.saveCart()
         return item}
@@ -32,20 +24,6 @@ export class CartService {
   this.saveCart();
   return item;
 }
-  removeItem(product: Product, qty: number): CartItem[] {
-    if (this.cart.length > 0) {
-      for (let item of this.cart) {
-        if (item.product.id == product.id) {
-          if (item.qty - qty > 0) {
-            return this.deduct(item, qty);
-          } else {
-            return this.remove(item);
-          }
-        }
-      }
-    }
-    return this.cart;
-  }
   remove(cartItem: CartItem): CartItem[] {
     var index = this.cart.indexOf(cartItem);
     if (index !== -1) {
@@ -54,17 +32,6 @@ export class CartService {
     this.saveCart();
     return this.cart;
   }
-  increament(cartItem: CartItem, qty: number): CartItem[] {
-    cartItem.qty = cartItem.qty + qty;
-    this.saveCart();
-    return this.cart;
-  }
-  deduct(cartItem: CartItem, qty: number): CartItem[] {
-    cartItem.qty = cartItem.qty - qty;
-    this.saveCart();
-    return this.cart;
-  }
-
   clear() {
     this.cart = [];
     this.saveCart();
