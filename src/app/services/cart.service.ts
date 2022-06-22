9d2cf2f28455d14a68877224fce60d1f734c1839
ahmed <ahmedismailm85@gmail.com>
2022-06-22 08:14:39 +0200

    format doc

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -3,50 +3,45 @@ import { CartItem } from '../models/cartItem.model';
 import { Product } from '../models/product.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
-  cart: CartItem[] =JSON.parse(localStorage.getItem('cart')||'[]') ;
-  constructor() { 
-  }
+  cart: CartItem[] = JSON.parse(localStorage.getItem('cart') || '[]');
+  constructor() {}
   add(product: Product, qty: number): CartItem[] {
-    if (this.cart.length > 0){
-      
-      this.cart.forEach(function(item){
-        if (item.product.id == product.id){
+    if (this.cart.length > 0) {
+      for (let item of this.cart) {
+        if (item.product.id == product.id) {
           item.qty = item.qty + qty;
-          return item
-        }else{
-          return false
+          this.saveCart();
+          return this.cart;
         }
-        }
-      )
+      }
     }
-    this.cart.push({product:product, qty:qty})
-    this.saveCart()
-    return this.cart
+    this.cart.push({ product: product, qty: qty });
+    this.saveCart();
+    return this.cart;
   }
-  remove(cartItem :CartItem): CartItem[] {
+  remove(cartItem: CartItem): CartItem[] {
     var index = this.cart.indexOf(cartItem);
     if (index !== -1) {
       this.cart.splice(index, 1);
     }
-    this.saveCart()
-    return this.cart
+    this.saveCart();
+    return this.cart;
   }
-  clear(){
-    this.cart = []
-    this.saveCart()
-  }
-
-  getLength(): Number{
-    return this.cart.length
-  }
-  getCart():CartItem[]{
-    return this.cart
-  }
-  saveCart():void{
-    localStorage.setItem('cart',JSON.stringify(this.cart))
+  clear() {
+    this.cart = [];
+    this.saveCart();
   }
 
+  getLength(): Number {
+    return this.cart.length;
+  }
+  getCart(): CartItem[] {
+    return this.cart;
+  }
+  saveCart(): void {
+    localStorage.setItem('cart', JSON.stringify(this.cart));
+  }
 }
