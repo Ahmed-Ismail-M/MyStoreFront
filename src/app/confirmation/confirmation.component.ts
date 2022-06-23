b992ef26d09c489b086006f0e165f7ff3461d3d1
ahmed <ahmedismailm85@gmail.com>
2022-06-23 19:44:39 +0200

    add user and total to conf

--- src/app/confirmation/confirmation.component.ts
+++ src/app/confirmation/confirmation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-confirmation',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./confirmation.component.css']
 })
 export class ConfirmationComponent implements OnInit {
-
+  user: User = {full_name:'', address:"", cc:""}
+  total: string= '0'
   constructor() { }
 
   ngOnInit(): void {
